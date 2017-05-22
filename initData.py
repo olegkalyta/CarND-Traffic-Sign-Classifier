de5c1221712d5cbe46b79cdbdd7cf9a158a9aecf
Oleg Kalyta <2xyligan@gmail.com>
2017-05-22 19:42:46 +0300

    fixed init function

--- initData.py
+++ initData.py
@@ -2,34 +2,22 @@ import pickle
 
 
 def init():
-    training_file = 'data/train.p'
-    validation_file = 'data/valid.p'
-    # testing_file = 'data/test.p'
-
-    with open(training_file, mode='rb') as f:
+    with open('data/train.p', mode='rb') as f:
         train = pickle.load(f)
-    with open(validation_file, mode='rb') as f:
+    with open('data/valid.p', mode='rb') as f:
         valid = pickle.load(f)
+    with open('data/test.p', mode='rb') as f:
+        test = pickle.load(f)
 
     y_train = train['labels']
     y_valid = valid['labels']
+    y_test = test['labels']
 
     with open('data/normalized/x_train_optimized.txt', mode='rb') as f:
         X_train_optimized = pickle.load(f)
     with open('data/normalized/x_valid_optimized.txt', mode='rb') as f:
         X_valid_optimized = pickle.load(f)
+    with open('data/normalized/x_test_optimized.txt', mode='rb') as f:
+        X_test_optimized = pickle.load(f)
 
-    with open(training_file, mode='rb') as f:
-        train = pickle.load(f)
-    with open(validation_file, mode='rb') as f:
-        valid = pickle.load(f)
-
-    y_train = train['labels']
-    y_valid = valid['labels']
-
-    with open('data/normalized/x_train_optimized.txt', mode='rb') as f:
-        X_train_optimized = pickle.load(f)
-    with open('data/normalized/x_valid_optimized.txt', mode='rb') as f:
-        X_valid_optimized = pickle.load(f)
-
-    return [y_train, y_valid, X_train_optimized, X_valid_optimized]
+    return [X_train_optimized, X_valid_optimized, X_test_optimized, y_train, y_valid, y_test]
