6adba1f183ebbc18ed9e7baa04c1fc4ba52a5bed
Oleg Kalyta <2xyligan@gmail.com>
2017-06-11 23:40:26 +0300

    refactored lenet

--- lenet.py
+++ lenet.py
@@ -1,55 +1,51 @@
 import tensorflow as tf
 from tensorflow.contrib.layers import flatten
 
+
 def LeNet(x):
-    # Arguments used for tf.truncated_normal, randomly defines variables for the weights and biases for each layer
     mu = 0
     sigma = 0.08
 
-    # SOLUTION: Layer 1: Convolutional. Input = 32x32x3. Output = 28x28x6.
+    def fc(input, input_size, output_size):
+        fc_W = tf.Variable(tf.truncated_normal(shape=(input_size, output_size), mean=mu, stddev=sigma), name="w")
+        fc_b = tf.Variable(tf.zeros(output_size), name="b")
+        return tf.matmul(input, fc_W) + fc_b
+
+    # Convolutional Layer 1. Input = 32x32x3. Output = 28x28x6.
     with tf.name_scope("conv1"):
-      conv1_W = tf.Variable(tf.truncated_normal(shape=(5, 5, 3, 6), mean=mu, stddev=sigma))
-      conv1_b = tf.Variable(tf.zeros(6))
-      conv1 = tf.nn.conv2d(x, conv1_W, strides=[1, 1, 1, 1], padding='VALID') + conv1_b
-      conv1 = tf.nn.relu(conv1)
+        conv1_W = tf.Variable(tf.truncated_normal(shape=(5, 5, 3, 6), mean=mu, stddev=sigma), name="w")
+        conv1_b = tf.Variable(tf.zeros(6), name="b")
+        conv1 = tf.nn.conv2d(x, conv1_W, strides=[1, 1, 1, 1], padding='VALID') + conv1_b
+        conv1 = tf.nn.relu(conv1)
 
-    # SOLUTION: Pooling. Input = 28x28x6. Output = 14x14x6.
+    # Pooling Layer 1. Input = 28x28x6. Output = 14x14x6.
     with tf.name_scope("pool1"):
-      conv1 = tf.nn.max_pool(conv1, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='VALID')
+        conv1 = tf.nn.max_pool(conv1, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='VALID')
 
-    # SOLUTION: Layer 2: Convolutional. Output = 10x10x16.
+    # Convolutional Layer 2. Output = 10x10x16.
     with tf.name_scope("conv2"):
-      conv2_W = tf.Variable(tf.truncated_normal(shape=(5, 5, 6, 16), mean=mu, stddev=sigma))
-      conv2_b = tf.Variable(tf.zeros(16))
-      conv2 = tf.nn.conv2d(conv1, conv2_W, strides=[1, 1, 1, 1], padding='VALID') + conv2_b
-      conv2 = tf.nn.relu(conv2)
+        conv2_W = tf.Variable(tf.truncated_normal(shape=(5, 5, 6, 16), mean=mu, stddev=sigma), name="w")
+        conv2_b = tf.Variable(tf.zeros(16), name="b")
+        conv2 = tf.nn.conv2d(conv1, conv2_W, strides=[1, 1, 1, 1], padding='VALID') + conv2_b
+        conv2 = tf.nn.relu(conv2)
 
-    # SOLUTION: Pooling. Input = 10x10x16. Output = 5x5x16.
+    # Pooling. Input = 10x10x16. Output = 5x5x16.
     with tf.name_scope("pool2"):
-      conv2 = tf.nn.max_pool(conv2, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='VALID')
+        conv2 = tf.nn.max_pool(conv2, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='VALID')
 
-    # SOLUTION: Flatten. Input = 5x5x16. Output = 400.
+    # Flatten. Input = 5x5x16. Output = 400.
     with tf.name_scope("flatten"):
-      fc0 = flatten(conv2)
+        fc0 = flatten(conv2)
 
-    # SOLUTION: Layer 3: Fully Connected. Input = 400. Output = 120.
     with tf.name_scope("fc1"):
-      fc1_W = tf.Variable(tf.truncated_normal(shape=(400, 120), mean=mu, stddev=sigma))
-      fc1_b = tf.Variable(tf.zeros(120))
-      fc1 = tf.matmul(fc0, fc1_W) + fc1_b
-      fc1 = tf.nn.relu(fc1)
+        fc1 = fc(fc0, 400, 120)
+        fc1 = tf.nn.relu(fc1)
 
-    # SOLUTION: Layer 4: Fully Connected. Input = 120. Output = 84.
     with tf.name_scope("fc2"):
-      fc2_W = tf.Variable(tf.truncated_normal(shape=(120, 84), mean=mu, stddev=sigma))
-      fc2_b = tf.Variable(tf.zeros(84))
-      fc2 = tf.matmul(fc1, fc2_W) + fc2_b
-      fc2 = tf.nn.relu(fc2)
+        fc2 = fc(fc1, 120, 84)
+        fc2 = tf.nn.relu(fc2)
 
-    # SOLUTION: Layer 5: Fully Connected. Input = 84. Output = 43.
     with tf.name_scope("fc3"):
-      fc3_W = tf.Variable(tf.truncated_normal(shape=(84, 43), mean=mu, stddev=sigma))
-      fc3_b = tf.Variable(tf.zeros(43))
-      logits = tf.matmul(fc2, fc3_W) + fc3_b
+        logits = fc(fc2, 84, 43)
 
     return logits
